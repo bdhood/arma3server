793f6c0e20833de31c7cd9ef64d79069ef046d54
Ben Hood <hoodb2456@gmail.com>
2023-03-19 11:30:51 -0500

    run as non-root

--- image/entrypoint.sh
+++ image/entrypoint.sh
@@ -2,18 +2,11 @@
 
 set -ex
 
-chown -R steam:steam /root/Steam
-chown -R steam:steam /home/steam/.local/share
+cd "${HOME}/Steam"
+if [ ! -f "steamcmd.sh" ]; then
+    curl -sqL "https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz" | tar zxvf -
+fi
+./steamcmd.sh +force_install_dir ./arma3 +login "${STEAM_USERNAME}" "${STEAM_PASSWORD}" +app_update 233780 +quit
 
-su steam -c sh << EOF
-    whoami
-    ls -al /home/steam/.local
-    ls -al /home/steam/.local/share
-    steamcmd +force_install_dir /home/steam/Steam/arma3 +login "${STEAM_USERNAME}" "${STEAM_PASSWORD}" +app_update 233780 +quit
-EOF
-
-chown -R steam:steam /home/steam/Steam/arma3
-su steam -c sh << EOF
-    cd "/home/steam/Steam/arma3"
-    ./arma3server_x64 -name=server -config=/home/steam/server.cfg -world=empty
-EOF
+cd "./arma3"
+./arma3server_x64 "$@"
--- run.ps1
+++ run.ps1
@@ -1,8 +1,8 @@
 
-New-Item -ErrorAction Ignore -ItemType directory -Path volumes\\steamcmd
+New-Item -ErrorAction Ignore -ItemType directory -Path volumes\\Steam
 New-Item -ErrorAction Ignore -ItemType directory -Path volumes\\arma3
 docker build .\\image -t arma3server:latest
 if ($LastExitCode -ne 0) {
     exit 1
 }
-docker run -it -v "$(pwd)\\volumes\\steamcmd:/home/steam/Steam" -v "$(pwd)\\volumes\\arma3:/home/steam/Steam/arma3" --env-file=.env  -p 2302:2302/udp -p 2303:2303/udp -p 2304:2304/udp -p 2305:2305/udp -p 2306:2306/udp "docker.io/library/arma3server:latest"
+docker run -it -v "$(pwd)\\volumes\\Steam:/home/steam/Steam" -v "$(pwd)\\volumes\\arma3:/home/steam/Steam/arma3" --env-file=.env  -p 2302:2302/udp -p 2303:2303/udp -p 2304:2304/udp -p 2305:2305/udp -p 2306:2306/udp "docker.io/library/arma3server:latest" -name=server -config=/home/steam/server.cfg -world=empty
